6dea361bf0c5097fc0ef28b608907744c903ffa9
Aliecake <neyabenz@gmail.com>
2020-07-27 13:31:13 -0400

    Updates

---
+++ precache-manifest.4b366a2082dfe2fbba357dacf2b15613.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50cc73058e63cf7d305271b843ae9d2d",
+    "url": "/reactGalleryApp/index.html"
+  },
+  {
+    "revision": "e63bbe35ee7ec116d6cc",
+    "url": "/reactGalleryApp/static/css/main.fee8c568.chunk.css"
+  },
+  {
+    "revision": "dbd47156aa23d8f1624e",
+    "url": "/reactGalleryApp/static/js/2.42b7a112.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/reactGalleryApp/static/js/2.42b7a112.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e63bbe35ee7ec116d6cc",
+    "url": "/reactGalleryApp/static/js/main.a3f1c74e.chunk.js"
+  },
+  {
+    "revision": "0ffb22205055685832bb",
+    "url": "/reactGalleryApp/static/js/runtime-main.206f64a0.js"
+  }
+]);
